2d0ef9bd4e5e926feff2cfb59e9d8d22fa5b0de9
laura-minery <lgonzalez@mineryreport.com>
2022-07-11 19:29:51 +0200

    .

--- www/home.php
+++ www/home.php
@@ -7,15 +7,5 @@
             <h3 class="texto-primario">rellena los datos de tu moto</h3>
             <p>Explicación primera parte de tasación. Rellena los datos de tu moto. tinciLorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tinbortis.</p>
         </div>
-        <div class="pb-3 bloque-icono px-bloque-icono" data-aos="fade-up">
-            <img class="icono-tasador" src="./assets/8 - Home-tasacion rellena tus datos.svg" alt="Icono Tasacion">
-            <h3 class="texto-primario">rellena tus datos</h3><br class="ocultar-movil">
-            <p>Explicación segunda parte de tasación. Rellenas tus datos. dolor sit dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis.</p>
-        </div>
-        <div class="pb-3 bloque-icono" data-aos="fade-up">
-            <img class="icono-tasador" src="./assets/9 - Home-tasacion mandamos tasacion.svg" alt="Icono Tasacion">
-            <h3 class="texto-primario">te mandamos la tasación</h3>
-            <p>Explicación tercera parte de tasación. Un experto tasador tasa tu moto.... sit amet, cmagna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis.</p>
-        </div>
     </div>
 </div>
